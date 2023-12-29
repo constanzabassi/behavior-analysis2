efa78c18218f1d75ba8fc0eef55e5dba25d79b7d
constanzabassi <cdb66@pitt.edu>
2023-12-28 20:13:51 -0500

    updated align_info

--- align_behavior_data.m
+++ align_behavior_data.m
@@ -6,8 +6,11 @@ imaging_array = [imaging(good_trials).movement_in_imaging_time]; %convert to arr
 
 %find the trial with the smallest amount of frames
 maze_length= cellfun(@length,{imaging_array.maze_frames});
-stim_onset = cellfun(@(x) find(x,1, 'first'),{imaging_array.stimulus},'UniformOutput',false); %find first one in stimulus
-min_length = min(maze_length - [stim_onset{1,:}]);
+temp_reward = cellfun(@(x) find(x==1),{imaging_array.stimulus},'UniformOutput',false);
+stim_onset = cellfun(@min,temp_reward,'UniformOutput',false); %find first one in stimulus to determine stimulus onset
+shortest_maze_length = min(maze_length - [stim_onset{1,:}]);
+min_length_stim = min([stim_onset{1,:}])-1; %min number of frames in front of stimulus onset during maze
+
 
 max_length_reward = min(cellfun(@length,{imaging_array.reward_frames})); %24/25 or 30 frames between maze and ITI
 reward_onset = cellfun(@(x) find(x == 1),{imaging_array.is_reward},'UniformOutput',false); %reward onset based on all frames in trial
@@ -15,19 +18,23 @@ reward_trial = cellfun(@(x) ~isempty(x),reward_onset,'UniformOutput',false);
 reward_trial = find([reward_trial{1,:}] == 1); %of good trials which ones have reward
 min_length_reward = min([[reward_onset{1,:}] - cellfun(@min,{imaging_array(reward_trial).reward_frames})]); %min number of frames in front of reward before the end of maze
 
-align_info.stim_onset = stim_onset;
+align_info.stim_onsets = [stim_onset{1,:}];
 align_info.maze_length = maze_length;
-align_info.min_length = min_length;
+align_info.min_length = shortest_maze_length;
+align_info.stim_onset = min_length_stim+1;
 
-align_info.reward_onset = reward_onset;
+align_info.reward_onsets = [reward_onset{1,:}];
 align_info.max_length_reward = max_length_reward;
-align_info.min_length_reward = min_length_reward;
+align_info.reward_onset = min_length_reward+1;
 
 if strcmp(alignment_type,'stimulus' )
-% 1) align data based on stimulus onset (include prededing maze? couple frames)
+% 1) align data based on stimulus onset (include preceding maze- couple frames)
     for vr_trials = 1:length(good_trials)
         t = good_trials(vr_trials);
-        frames_to_include = stim_onset{1,vr_trials}:stim_onset{1,vr_trials}+min_length; %currently stim onset is frame 1
+        if t == 72
+        t
+        end
+        frames_to_include = stim_onset{1,vr_trials}-min_length_stim:stim_onset{1,vr_trials}+shortest_maze_length-min_length_stim; %currently stim onset is frame 1
         if strcmp(data_type,'dff')
             aligned_imaging(vr_trials,:,:) = imaging(good_trials(vr_trials)).dff(:,frames_to_include);
         elseif strcmp(data_type,'z_dff')
@@ -38,10 +45,12 @@ if strcmp(alignment_type,'stimulus' )
        
     end
 elseif strcmp(alignment_type,'turn')
+    frames_around = 30;
+    align_info.turn_onset = frames_around+1;
 % 2) align data based on maze offset/turn (1 sec pre and post)
     for vr_trials = 1:length(good_trials) 
             t = vr_trials;
-            frames_to_include = imaging_array(t).turn_frame-30:imaging_array(t).turn_frame+30; %turn onset is frame 31
+            frames_to_include = imaging_array(t).turn_frame-frames_around :imaging_array(t).turn_frame+frames_around ; %turn onset is frame 31
             if strcmp(data_type,'dff')
                 aligned_imaging(vr_trials,:,:) = imaging(good_trials(vr_trials)).dff(:,frames_to_include);
             elseif strcmp(data_type,'z_dff')
