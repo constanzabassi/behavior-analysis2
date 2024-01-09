00558d4df7d7d4d90f08424aaa8b705818f95ff7
constanzabassi <cdb66@pitt.edu>
2024-01-08 23:04:29 -0500

    update make_conditionheatmaps_celltypes

--- make_conditionheatmaps_celltypes.m
+++ make_conditionheatmaps_celltypes.m
@@ -1,11 +1,31 @@
-function make_conditionheatmaps_celltypes(imaging_st,alignment_type,data_type,sorting_type,min_max,all_celltypes,celltype_id)
-for m = 1:length(imaging_st)
-    imaging = imaging_st{1,m};
-    [all_conditions, condition_array_trials] = divide_trials (imaging); %divide trials into all possible conditions
-    possible_celltypes = fieldnames(all_celltypes{1,1});
-    chosen_celltype = contains(possible_celltypes,celltype_id);
-    celltypes_permouse = all_celltypes{1,m}.(possible_celltypes{chosen_celltype});
-    imaging_conditions = align_data_per_condition(imaging,all_conditions,data_type,alignment_type,celltypes_permouse); %align data to event
-    figure(m*100);clf;
-    make_condition_heatmaps (imaging_conditions,min_max,sorting_type,all_conditions,alignment_type); %plot mean for each condition
-end
+function make_conditionheatmaps_celltypes(imaging_st,cat_imaging,alignment_type,data_type,sorting_type,min_max,all_celltypes,celltype_id)
+    if isempty(cat_imaging) %per dataset
+        for m = 1:length(imaging_st)
+            imaging = imaging_st{1,m};
+            [all_conditions, condition_array_trials] = divide_trials (imaging); %divide trials into all possible conditions
+            possible_celltypes = fieldnames(all_celltypes{1,1});
+            chosen_celltype = contains(possible_celltypes,celltype_id);
+            celltypes_permouse = all_celltypes{1,m}.(possible_celltypes{chosen_celltype});
+            imaging_conditions = align_data_per_condition(imaging,all_conditions,data_type,alignment_type,[],celltypes_permouse); %align data to event
+            figure(m*100);clf;
+            make_condition_heatmaps (imaging_conditions,min_max,sorting_type,all_conditions,alignment_type); %plot mean for each condition
+        end
+    else %across all datasets!
+
+        % align data across all datasets using same set of info (if
+        % concatenated together)
+        align_info = find_align_info(cat_imaging,30);
+
+        possible_celltypes = fieldnames(all_celltypes{1,1});
+        chosen_celltype = contains(possible_celltypes,celltype_id);
+        for m = 1:length(all_celltypes)
+            imaging = imaging_st{1,m};
+            [all_conditions, condition_array_trials] = divide_trials (imaging); %divide trials into all possible conditions
+            celltypes_permouse{m} = all_celltypes{1,m}.(possible_celltypes{chosen_celltype});
+            imaging_conditions{m} = align_data_per_condition(imaging,all_conditions,data_type,alignment_type,align_info,celltypes_permouse{m}); %align data to event         
+        end
+        [mean_conditions,imaging_conditions_updated] = find_mean_imaging_conditions(imaging_conditions);
+        figure(101);clf;
+        make_condition_heatmaps (imaging_conditions_updated,min_max,sorting_type,all_conditions,alignment_type); %plot mean for each condition
+    end
+
