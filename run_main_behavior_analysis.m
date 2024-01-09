63d0e7f60d806f4903e8e92f1ab0c51f4c9cefb8
constanzabassi <cdb66@pitt.edu>
2024-01-08 23:04:09 -0500

    update run_main

--- run_main_behavior_analysis.m
+++ run_main_behavior_analysis.m
@@ -6,7 +6,7 @@ info.server = {'V:','V:','V:','V:','V:','V:'};%\\runyan-fs-01\Runyan2';
 info.savepath = 'V:/Connie/results/VR';%'Y:\Connie\results\PVSOM_opto\lab_meetingmay2023'; %'Y:\Connie\results\opto_figs_sfn'
 
 %% 2)pool imaging data structure from multiple datasets and organize it
-[all_celltypes,imaging_st,mouse] = pool_imaging(info.mouse_date,info.server);
+[all_celltypes,imaging_st,mouse,cat_imaging] = pool_imaging(info.mouse_date,info.server);
  
 %organize so that all mice are within one variable  
 [num_cells,sorted_cells] = organize_pooled_celltypes(mouse,all_celltypes);
@@ -17,8 +17,19 @@ min_max = [-0.5 2];
 sorting_type = 1; % 1 by time, any other number by max value
 data_type = 'z_dff';% 'dff', 'z_dff', else it's deconvolved
 alignment_type = 'stimulus'; %'reward','turn','stimulus'
-cell_type = 'pv';
+cell_type = 'pyr';
 
-make_conditionheatmaps_celltypes(imaging_st,alignment_type,data_type,sorting_type,min_max,all_celltypes,cell_type);
+make_conditionheatmaps_celltypes(imaging_st,[],alignment_type,data_type,sorting_type,min_max,all_celltypes,cell_type);
 
 %% 4) same as 3 but put them together into single plots for each dataset
+
+min_max = [-0.5 2];
+sorting_type = 1; % 1 by time, any other number by max value
+data_type = 'z_dff';% 'dff', 'z_dff', else it's deconvolved
+alignment_type = 'stimulus'; %'reward','turn','stimulus'
+cell_type = 'som';
+
+make_conditionheatmaps_celltypes(imaging_st,cat_imaging,alignment_type,data_type,sorting_type,min_max,all_celltypes,cell_type);
+
+%% 5) make heatmaps per condition but sort them using particular condition
+
