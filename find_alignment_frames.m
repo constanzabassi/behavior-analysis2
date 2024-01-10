6484692a4cddd1757717b5096d34e931bc01026f
constanzabassi <cdb66@pitt.edu>
2024-01-10 15:02:49 -0500

    fix padding size

--- find_alignment_frames.m
+++ find_alignment_frames.m
@@ -1,6 +1,6 @@
-function frames = find_alignment_frames (alignment_frames,event_id)
+function frames = find_alignment_frames (alignment_frames,event_id,left_padding,right_padding)
 
-frames = zeros(size(alignment_frames, 2), sum(left_padding) + sum(right_padding) + length(even_id));
+frames = zeros(size(alignment_frames, 2), sum(left_padding(event_id)) + sum(right_padding(event_id)) + length(event_id));
 for i=1:size(alignment_frames, 2)
     temp_frames = [];
     for events = event_id;%1:size(alignment_frames,1)
