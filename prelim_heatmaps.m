b01d589da2d9db2dd8aab9b35c92c4371184dcb8
constanzabassi <cdb66@pitt.edu>
2024-01-11 00:28:01 -0500

    update prelim heatmaps with structured inputs

--- prelim_heatmaps.m
+++ prelim_heatmaps.m
@@ -7,12 +7,12 @@ addpath(genpath(selected_folder))
 ex_trial = 197;
 data = imaging(ex_trial).z_dff;
 
-min_max = [-0.5 2];
-sorting_type = 1; %1 by time, any other number by max value
+plot_info.min_max = [-0.5 2];
+plot_info.sorting_type = 1; %1 by time, any other number by max value
 figure(1);clf;
 hold on
 title(['Example trial: ' num2str(ex_trial)])
-make_heatmap(data,min_max,sorting_type,1);
+make_heatmap(data,plot_info,1);
 plot(rescale(imaging(ex_trial).movement_in_imaging_time.stimulus,0,size(data,1)),'-w');
 plot(rescale(imaging(ex_trial).movement_in_imaging_time.is_reward,0,size(data,1)),'-r');
 plot(rescale(imaging(ex_trial).movement_in_imaging_time.in_ITI,0,size(data,1)),'-g');
@@ -26,42 +26,46 @@ hold off
 
 %make plots based on conditions align based on onset first 
 %1) align data (trials,cells,frames)
+alignment.data_type = 'z_dff';
 align_info = find_align_info(imaging,30); %find min # of frames per event
-[aligned_stimulus,imaging_array,align_info] = align_behavior_data_prelim (imaging,align_info,'z_dff','stimulus');
-[aligned_turn,imaging_array,align_info] = align_behavior_data_prelim (imaging,align_info,'z_dff','turn');
-[aligned_reward,imaging_array,align_info] = align_behavior_data_prelim (imaging,align_info,'z_dff','reward');
+alignment.type = 'stimulus';
+[aligned_stimulus,imaging_array,align_info] = align_behavior_data_prelim (imaging,align_info,alignment);
+alignment.type = 'turn';
+[aligned_turn,imaging_array,align_info] = align_behavior_data_prelim (imaging,align_info,alignment);
+alignment.type = 'reward';
+[aligned_reward,imaging_array,align_info] = align_behavior_data_prelim (imaging,align_info,alignment);
 
 figure(2);clf;
 tiledlayout(1,4,"TileSpacing",'compact')
 nexttile
 hold on
 title('first stimulus onset')
-make_heatmap(squeeze(mean(aligned_stimulus,1)),min_max,sorting_type,align_info.stimulus_onset,align_info.stimulus_onset);
+make_heatmap(squeeze(mean(aligned_stimulus,1)),plot_info,align_info.stimulus_onset,align_info.stimulus_onset);
 hold off
 
 nexttile
 hold on
 title('turn onset')
-make_heatmap(squeeze(mean(aligned_turn,1)),min_max,sorting_type,align_info.turn_onset,align_info.turn_onset);
+make_heatmap(squeeze(mean(aligned_turn,1)),plot_info,align_info.turn_onset,align_info.turn_onset);
 hold off
 
 nexttile
 hold on
 title('reward onset')
-make_heatmap(squeeze(mean(aligned_reward,1)),min_max,sorting_type,align_info.reward_onset,align_info.reward_onset);
+make_heatmap(squeeze(mean(aligned_reward,1)),plot_info,align_info.reward_onset,align_info.reward_onset);
 hold off
 
 nexttile
 hold on
 title('stimulus and turn onsets')
-make_heatmap([squeeze(mean(aligned_stimulus,1)),NaN(size(aligned_stimulus,2),3),squeeze(mean(aligned_turn,1))],min_max,sorting_type,align_info.stimulus_onset,align_info.stimulus_onset,align_info.min_length+align_info.turn_onset+3); %+ length of maze and +3 for NaN
+make_heatmap([squeeze(mean(aligned_stimulus,1)),NaN(size(aligned_stimulus,2),3),squeeze(mean(aligned_turn,1))],plot_info,align_info.stimulus_onset,align_info.stimulus_onset,align_info.min_length+align_info.turn_onset+3); %+ length of maze and +3 for NaN
 hold off
 
 %% 3) divide data into correct/incorrect, left/right, stim/no stim and plot the mean
 [all_conditions, condition_array_trials] = divide_trials (imaging);
-alignment_type = 'stimulus'; %'reward','turn','stimulus'
-imaging_conditions = align_data_per_condition(imaging,all_conditions,'z_dff',alignment_type,[]);
+alignment.type = 'stimulus'; %'reward','turn','stimulus'
+imaging_conditions = align_data_per_condition(imaging,all_conditions,alignment,[]);
 
 figure(4);clf;
-make_condition_heatmaps (imaging_conditions,min_max,sorting_type,all_conditions,alignment_type,[]); %plot mean for each condition
+make_condition_heatmaps (imaging_conditions,plot_info,all_conditions,alignment,[]); %plot mean for each condition
 
