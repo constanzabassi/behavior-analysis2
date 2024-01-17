41b75116acfe928bc29bde16541d19a766fa13a3
constanzabassi <cdb66@pitt.edu>
2024-01-17 10:15:13 -0500

    update run main w alignment input

--- run_main_behavior_analysis.m
+++ run_main_behavior_analysis.m
@@ -2,9 +2,9 @@
 addpath(genpath('C:\Code\Github\behavior-analysis'))
 
 %1) choose datasets
-info.mouse_date={'HA11-1R/2023-05-05','HA11-1R/2023-04-13','HA10-1L/2023-04-10','HA10-1L/2023-04-17','HA2-1L/2023-04-12','HA2-1L/2023-05-05','HA1-00/2023-06-29','HE4-1L1R\2023-08-24'}; %mice with behavioral responses ,'GS9-1L/2023-01-16','GS8-00/2022-12-20','HA13-1L/2023-02-24','GE3-00/2022-10-20','HA10-1L/2023-03-27-session2'
+info.mouse_date={'HA11-1R/2023-05-05','HA11-1R/2023-04-13','HA2-1L/2023-04-12','HA2-1L/2023-05-05','HA1-00/2023-06-29','HA1-00/2023-08-28','HE4-1L1R\2023-08-21','HE4-1L1R\2023-08-24'}; %mice with behavioral responses ,'GS9-1L/2023-01-16','GS8-00/2022-12-20','HA13-1L/2023-02-24','GE3-00/2022-10-20','HA10-1L/2023-03-27-session2'
 
-info.server = {'V:','V:','V:','V:','V:','V:','V:','W:'};%\\runyan-fs-01\Runyan2';
+info.server = {'V:','V:','V:','V:','V:','W:','W:','W:'};%\\runyan-fs-01\Runyan2';
 info.savepath = 'V:/Connie/results/VR';%'Y:\Connie\results\PVSOM_opto\lab_meetingmay2023'; %'Y:\Connie\results\opto_figs_sfn'
 
 %% 2)pool imaging data structure from multiple datasets and organize it
@@ -29,9 +29,12 @@ make_conditionheatmaps_celltypes(imaging_st,cat_imaging,alignment,plot_info,all_
 
 
 %% 4) plot invididual mice average across conditions with concatenated alignment
-ex_imaging = imaging_st{1,7};
+ex_imaging = imaging_st{1,8};
+alignment.data_type = 'z_dff';% 'dff', 'z_dff', else it's deconvolved
+alignment.type = 'all'; %'reward','turn','stimulus','ITI'
+
 [align_info,alignment_frames,left_padding,right_padding] = find_align_info (ex_imaging,30);
-[aligned_imaging,imaging_array,align_info] = align_behavior_data (ex_imaging,align_info,alignment_frames,left_padding,right_padding,data_type,'all');
+[aligned_imaging,imaging_array,align_info] = align_behavior_data (ex_imaging,align_info,alignment_frames,left_padding,right_padding,alignment);
 event_onsets = determine_onsets(left_padding,right_padding,[1:6]);
 [all_conditions, condition_array_trials] = divide_trials (ex_imaging); %divide trials into all possible conditions
 figure(88);clf;
@@ -72,10 +75,6 @@ if ~isempty(save_data_directory)
     saveas(90,[image_string '_datasets.fig']);
     saveas(90,[image_string '_datasets.pdf']);
 end
-
-
-
-
 
 %% 6) trying spatial binning
 alignment.data_type = 'z_dff';% 'dff', 'z_dff', else it's deconvolved
