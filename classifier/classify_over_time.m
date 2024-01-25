91176df690bf9f8c09c8164d21b05ce1f4153e03
constanzabassi <cdb66@pitt.edu>
2024-01-25 16:09:46 -0500

    update classify over time mdl output

--- classifier/classify_over_time.m
+++ classifier/classify_over_time.m
@@ -9,14 +9,16 @@ for t = 1:length(timepoints)
     X = get_X (aligned_data, mdl_param,selected_frames(t));
 
     %call classifier model
-    [~, accuracy(t)] = trainClassifier_cb(X, Y,[1;2],'SVM'); %accuracy(mouse,t)
+    [~, accuracy(t),mdl{t}] = trainClassifier_cb(X, Y,[1;2],'SVM'); %accuracy(mouse,t)
     %repeat but with suffled Y labels
     Y_shuff=Y(randperm(length(Y)));
-    [~, shuff_accuracy(t)] = trainClassifier_cb(X, Y_shuff,[1;2],'SVM'); 
+    [~, shuff_accuracy(t),shuff_mdl{t}] = trainClassifier_cb(X, Y_shuff,[1;2],'SVM'); 
 
 end
 %% STORE VARIABLES 
 output.accuracy=accuracy; %in Christians accuracy is (datasets,bins)
 output.shuff_accuracy=shuff_accuracy; 
+output.mdl = mld;
+output.shuff_mdl = shuff_mdl;
 output.mdl_param = mdl_param;
 
