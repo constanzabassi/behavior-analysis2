ec8f668a2ec71ba07e5dcef2f1e685fb93a99b0a
constanzabassi <cdb66@pitt.edu>
2024-01-29 14:59:24 -0500

    update subsample_fun

--- subsample_fun.m
+++ subsample_fun.m
@@ -1,6 +1,3 @@
 function sub_data = subsample_fun(data,num_observations_needed)
 
-for i = 1:num_iterations
-    sub_data = randsample(data,num_observations_needed);
-%     velocity_bin_trials_subsampled{m,b,i} = sub_data;
-end
+sub_data = randsample(data,num_observations_needed);
