cfa607419aa97f2d95c5caeadf447ef809813ef5
constanzabassi <cdb66@pitt.edu>
2024-01-29 14:59:04 -0500

    update run classifier to plot and save data

--- classifier/run_classifier.m
+++ classifier/run_classifier.m
@@ -1,8 +1,8 @@
 %% SVM predict choice using cell type activity
-function output = run_classifier(imaging_st,all_celltypes,mdl_param, alignment,plot_info)
+function [output, output_mat] = run_classifier(imaging_st,all_celltypes,mdl_param, alignment,plot_info,info)
 possible_celltypes = fieldnames(all_celltypes{1,1});
 
-for it = 1:num_iterations
+for it = 1:mdl_param.num_iterations
     for m = 1:8
         count = 0;
         m
@@ -19,9 +19,12 @@ for it = 1:num_iterations
             
             % [train_imaging, test_imaging,all_trials] = split_imaging_train_test_cb(ex_imaging,condition_array_trials,0.7,0,0);
             
-            mdl_param.mdl_cells = all_celltypes{1,m}.(possible_celltypes{ce}); %which cell type to use
+%             mdl_param.mdl_cells = all_celltypes{1,m}.(possible_celltypes{ce}); %which cell type to use
+
             %subsample cells!
-            sub_data = subsample_fun(data,num_observations_needed)
+            num_observations_needed = min(cellfun(@length,struct2cell(all_celltypes{1,m}))); %min number of cells 
+            sub_data = subsample_fun(all_celltypes{1,m}.(possible_celltypes{ce}),num_observations_needed);
+            mdl_param.mdl_cells = sub_data;
             
 %             %use training data
 %             % [align_info,alignment_frames,left_padding,right_padding] = find_align_info (train_imaging,30);
@@ -29,7 +32,7 @@ for it = 1:num_iterations
 %             % [all_conditions_t, condition_array_trials_t] = divide_trials (train_imaging); %divide trials into all possible conditions
             
             if count == 1 %keep the trials the same across cell types!
-                selected_trials = subsample_trials_to_decorrelate_choice_and_category(ex_imaging,condition_array_trials);%(condition_array_trials_t);
+                selected_trials = subsample_trials_to_decorrelate_choice_and_category(condition_array_trials);%(condition_array_trials_t);
             else
                 mdl_param.selected_trials = selected_trials;
             end
@@ -40,23 +43,36 @@ for it = 1:num_iterations
             mdl_X = aligned_imaging(find(mdl_param.selected_trials),:,:);
             
             fprintf(['size Y : ' num2str(size(mdl_Y)) ' || size X : '  num2str(size(mdl_X)) '\n']);
-            output{m,ce} = classify_over_time(mdl_X,mdl_Y, mdl_param);
+            output{it,m,ce} = classify_over_time(mdl_X,mdl_Y, mdl_param);
         end
     end
 end
 
-%% make quick figure plots!
+%convert output to matrix form for easier indexing
+
+%% make quick figure plots! error across subsamples
 event_onsets = determine_onsets(left_padding,right_padding,[1:6]);
-for m = 1:8
+for m = 1:length(imaging_st)
 ex_mouse = m;
 figure(m);clf;
 subplot(2,1,1)
 for ce = 1:3
     hold on; 
     title(info.mouse_date{1,m})
+    
+    temp = [output{1:mdl_param.num_iterations,m,ce}];
+    output_mat{m,ce}.accuracy = cell2mat({temp.accuracy}'); %gives num subsample iterations x timepoints
+    output_mat{m,ce}.shuff_accuracy = cell2mat({temp.shuff_accuracy}');
+    % find squared error from the mean
+    SEM= std(output_mat{m,ce}.accuracy)/sqrt(size(output_mat{m,ce}.accuracy,1));
+    shadedErrorBar(1:size(output_mat{m,ce}.accuracy,2),mean(output_mat{m,ce}.accuracy,1), SEM, 'lineProps',{'color', plot_info.colors_celltype(ce,:)});
 
-    plot(output{ex_mouse,ce}.accuracy,'color',plot_info.colors_celltypes(ce,:));
-    plot(output{ex_mouse,ce}.shuff_accuracy,'color',[0.2 0.2 0.2]*ce);
+    SEM= std(output_mat{m,ce}.accuracy)/sqrt(size(output_mat{m,ce}.accuracy,1));
+    shadedErrorBar(1:size(output_mat{m,ce}.shuff_accuracy,2),mean(output_mat{m,ce}.shuff_accuracy,1), SEM, 'lineProps',{'color', [0.2 0.2 0.2]*ce});
+
+%     plot(output{ex_mouse,ce}.accuracy,'color',plot_info.colors_celltypes(ce,:));
+%     plot(output{ex_mouse,ce}.shuff_accuracy,'color',[0.2 0.2 0.2]*ce);
+
 
     for i = 1:length(event_onsets)
         xline(event_onsets(i),'--k','LineWidth',1.5)
@@ -71,8 +87,13 @@ hold off
 subplot(2,1,2)
 for ce = 1:3
     hold on; 
-    plot(smooth(output{ex_mouse,ce}.accuracy,3, 'boxcar'),'color',plot_info.colors_celltypes(ce,:));
-    plot(smooth(output{ex_mouse,ce}.shuff_accuracy,3, 'boxcar'),'color',[0.2 0.2 0.2]*ce);
+        
+    shadedErrorBar(1:size(output_mat{m,ce}.accuracy,2),smooth(mean(output_mat{m,ce}.accuracy,1),3, 'boxcar'), smooth(SEM,3, 'boxcar'), 'lineProps',{'color', plot_info.colors_celltype(ce,:)});
+
+    shadedErrorBar(1:size(output_mat{m,ce}.shuff_accuracy,2),smooth(mean(output_mat{m,ce}.shuff_accuracy,1),3, 'boxcar'), smooth(SEM,3, 'boxcar'), 'lineProps',{'color', [0.2 0.2 0.2]*ce});
+
+%     plot(smooth(output{ex_mouse,ce}.accuracy,3, 'boxcar'),'color',plot_info.colors_celltypes(ce,:));
+%     plot(smooth(output{ex_mouse,ce}.shuff_accuracy,3, 'boxcar'),'color',[0.2 0.2 0.2]*ce);
 
     for i = 1:length(event_onsets)
         xline(event_onsets(i),'--k','LineWidth',1.5)
@@ -85,3 +106,21 @@ end
 hold off
 movegui(gcf,'center')
 end
+
+mkdir([info.savepath '\SVM_' alignment.data_type '_' info.savestr])
+cd([info.savepath '\SVM_' alignment.data_type '_' info.savestr])
+save('output','output');
+save('output_mat','output_mat');
+%save_figs
+for m = 71:length(imaging_st)
+    str = info.mouse_date{1,m} ;
+    if ismember('/',info.mouse_date{1,m})
+        str = erase(info.mouse_date{1,m},'/');
+    else
+        str = erase(info.mouse_date{1,m},'\');
+    end
+    saveas(m,strcat('SVM_overtime_',str,'.svg'));
+    saveas(m,strcat('SVM_overtime_',str,'.png'));
+end
+
+
