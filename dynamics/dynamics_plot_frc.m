e0827797e19b7f6beec25d0edef7ac1b2b75dc86
constanzabassi <cdb66@pitt.edu>
2024-01-30 16:17:35 -0500

    update dynamics plots

--- dynamics/dynamics_plot_frc.m
+++ dynamics/dynamics_plot_frc.m
@@ -1,12 +1,3 @@
-[max_cel_avg,~, binss] = fraction_dynamics (imaging_st,alignment,3);
-event_onsets = determine_onsets(left_padding,right_padding,[1:6]);
-new_onsets = find(histcounts(event_onsets,binss));
+[max_cel_mode,freq,~, binss,new_onsets] = fraction_dynamics (imaging_st,alignment,3);
 
-frc=histcounts(max_cel_avg(2,:),1:length(binss))./381;
-figure(55);clf;
-hold on;
-plot(frc,'LineWidth',1.5)
-for i = 1:length(new_onsets)
-    xline(new_onsets(i),'--k','LineWidth',1.5)
-end
-hold off
+plot_frc_dynamics(max_cel_mode,all_celltypes,binss,new_onsets,plot_info, info,0); %last is save or not
