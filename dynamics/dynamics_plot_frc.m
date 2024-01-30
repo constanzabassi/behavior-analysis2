9c31bd85cdd827a179fe19c35e1d1a9c46d02a84
constanzabassi <cdb66@pitt.edu>
2024-01-30 16:40:01 -0500

    minor update

--- dynamics/dynamics_plot_frc.m
+++ dynamics/dynamics_plot_frc.m
@@ -1,3 +1,3 @@
-[max_cel_mode,freq,~, binss,new_onsets] = fraction_dynamics (imaging_st,alignment,3);
+[max_cel_mode,freq,~, binss,new_onsets] = fraction_dynamics (imaging_st,alignment,3); %last number is bin size
 
 plot_frc_dynamics(max_cel_mode,all_celltypes,binss,new_onsets,plot_info, info,0); %last is save or not
