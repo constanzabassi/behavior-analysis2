354ec4df229b104c68e1b2f4cf27529843689b36
constanzabassi <cdb66@pitt.edu>
2024-01-30 16:58:02 -0500

    create run_ROC

---
+++ ROC/run_ROC.m
@@ -0,0 +1,5 @@
+% CODE TO RUN ROC ANALYSIS BASED ON NAJAFI 2020 PAPER
+% use avetrialAlign_setVars to get the input vars.
+% function choicePref_ROC(traces_al_sm, ipsiTrs, contraTrs, makeplots, eventI_stimOn, useEqualNumTrs, doChoicePref, doshfl)
+% https://github.com/farznaj/imaging_decisionMaking_exc_inh/blob/master/imaging/avetrialAlign_caMean.m
+% https://github.com/farznaj/imaging_decisionMaking_exc_inh/blob/master/imaging/choicePref_ROC.m
