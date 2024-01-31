a6d56f7dcfe3fd9f49524a9c80f1abb28fbd18ea
constanzabassi <cdb66@pitt.edu>
2024-01-31 10:29:29 -0500

    update run_roc

--- ROC/run_ROC.m
+++ ROC/run_ROC.m
@@ -3,3 +3,5 @@
 % function choicePref_ROC(traces_al_sm, ipsiTrs, contraTrs, makeplots, eventI_stimOn, useEqualNumTrs, doChoicePref, doshfl)
 % https://github.com/farznaj/imaging_decisionMaking_exc_inh/blob/master/imaging/avetrialAlign_caMean.m
 % https://github.com/farznaj/imaging_decisionMaking_exc_inh/blob/master/imaging/choicePref_ROC.m
+alignment.data_type = 'dff';% 'dff', 'z_dff', else it's deconvolved
+alignment.type = 'all'; %'reward','turn','stimulus','ITI'
