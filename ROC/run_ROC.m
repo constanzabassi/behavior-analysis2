e889aa96e0fa21732ea4ab9b1ebcd38c0b8461f1
constanzabassi <cdb66@pitt.edu>
2024-02-02 12:55:40 -0500

    update run_roc to plot individual cells

--- ROC/run_ROC.m
+++ ROC/run_ROC.m
@@ -12,4 +12,13 @@ roc_mdl.shuff_num = 50;
 
 %% RUN ROC ACROSS DATASETS
 [roc_mdl] = compute_roc_choice (imaging_st,alignment,roc_mdl,info,1); %last value is whether to shuffle datasets or not
+%% PLOT EXAMPLE CELLS
+ex_mouse = 6;
+alignment.field_to_separate = [1,2]; %1) correct, 2) left turn, 3) condition, 4)is_stim_trial
+alignment.conditions = [3,4]; %of conditions separated list out which ones to plot
+alignment.data_type = 'z_dff';% 'dff', 'z_dff', else it's deconvolved
+alignment.type = 'all'; %'reward','turn','stimulus','ITI'
+alignment.number = [1:6]; %'reward','turn','stimulus'
+
+plot_example_cells(imaging_st,alignment,ex_mouse,roc_mdl); %sorts cells based on roc choice preference
 
