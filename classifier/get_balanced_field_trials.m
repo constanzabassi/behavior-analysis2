e57f079d8c3800f9d53d69e7f6620505e7c9910c
constanzabassi <cdb66@pitt.edu>
2024-02-06 12:19:06 -0500

    update get_balanced_fields
    makes even conditions 0 and odd 1 for 'condition'

--- classifier/get_balanced_field_trials.m
+++ classifier/get_balanced_field_trials.m
@@ -9,53 +9,55 @@ selected_trials = false(1, length(good_trials));
 virmen_trial_info = [imaging(good_trials).virmen_trial_info];
 
 if length(selected_field_num)==1
-condition = [virmen_trial_info.(fieldss{selected_field_num})];
-
-unique_cond = unique(condition);
-for c = 1:length(unique_cond)
-    conds{c} = find(condition == unique_cond(c));
-end
-
-
-% make sure that each of these groups is equally represented in
-% the training trials, to ensure that stimulus category and
-% behavioural choice are uncorrelated.
-
-smallest_set_size = min(cellfun(@length ,conds));
-lc_selected = randsample(conds{1}, smallest_set_size);
-rc_selected = randsample(conds{2}, smallest_set_size);
-selected_trials(lc_selected) = true;
-selected_trials(rc_selected) = true;
-
-smallest_set_size
+    condition = [virmen_trial_info.(fieldss{selected_field_num})];
+    
+    unique_cond = unique(condition);
+    for c = 1:length(unique_cond)
+        conds{c} = find(condition == unique_cond(c));
+    end
+    
+    
+    % make sure that each of these groups is equally represented in
+    % the training trials, to ensure that stimulus category and
+    % behavioural choice are uncorrelated.
+    
+    smallest_set_size = min(cellfun(@length ,conds));
+    lc_selected = randsample(conds{1}, smallest_set_size);
+    rc_selected = randsample(conds{2}, smallest_set_size);
+    selected_trials(lc_selected) = true;
+    selected_trials(rc_selected) = true;
+    
+    smallest_set_size
 else
-condition = [virmen_trial_info.(fieldss{selected_field_num(1)})]-1;
-condition2 = [virmen_trial_info.(fieldss{selected_field_num(2)})];
-
-
-unique_cond = unique(condition);
-for c = 1:length(unique_cond)
-    conds{c} = find(condition == unique_cond(c));
-end
-
-lc = condition & condition2;
-li = condition & ~condition2;
-rc = ~condition & condition2;
-ri = ~condition & ~condition2;
-
-% make sure that each of these groups is equally represented in
-% the training trials, to ensure that stimulus category and
-% behavioural choice are uncorrelated.
-
-smallest_set_size = min([nnz(lc), nnz(li), nnz(rc), nnz(ri)]);
-lc_selected = randsample(find(lc), smallest_set_size);
-li_selected = randsample(find(li), smallest_set_size);
-rc_selected = randsample(find(rc), smallest_set_size);
-ri_selected = randsample(find(ri), smallest_set_size);
-selected_trials(lc_selected) = true;
-selected_trials(li_selected) = true;
-selected_trials(rc_selected) = true;
-selected_trials(ri_selected) = true;
+    if strcmp(fieldss{selected_field_num(1)},'condition')
+        condition = rem([virmen_trial_info.(fieldss{selected_field_num(1)})],2);
+        condition2 = [virmen_trial_info.(fieldss{selected_field_num(2)})];
+    elseif strcmp(fieldss{selected_field_num(2)},'condition')
+        condition = [virmen_trial_info.(fieldss{selected_field_num(1)})];
+        condition2 = rem([virmen_trial_info.(fieldss{selected_field_num(2)})],2);
+    else
+        condition = [virmen_trial_info.(fieldss{selected_field_num(1)})];
+        condition2 = [virmen_trial_info.(fieldss{selected_field_num(2)})];
+    end
+    
+    lc = condition & condition2;
+    li = condition & ~condition2;
+    rc = ~condition & condition2;
+    ri = ~condition & ~condition2;
+    
+    % make sure that each of these groups is equally represented in
+    % the training trials, to ensure that stimulus category and
+    % behavioural choice are uncorrelated.
+    
+    smallest_set_size = min([nnz(lc), nnz(li), nnz(rc), nnz(ri)]);
+    lc_selected = randsample(find(lc), smallest_set_size);
+    li_selected = randsample(find(li), smallest_set_size);
+    rc_selected = randsample(find(rc), smallest_set_size);
+    ri_selected = randsample(find(ri), smallest_set_size);
+    selected_trials(lc_selected) = true;
+    selected_trials(li_selected) = true;
+    selected_trials(rc_selected) = true;
+    selected_trials(ri_selected) = true;
 
 end
 
