1c9697db4e3871f6553d40d09a856922c8011d46
constanzabassi <cdb66@pitt.edu>
2024-02-22 21:40:55 -0500

    update save_info

--- save_info.m
+++ save_info.m
@@ -1,8 +1,9 @@
-function save_info(info,all_celltypes,imaging_st,savepath)
+function save_info(info,all_celltypes,imaging_st,mouse,savepath)
 if ~isempty(savepath)
     mkdir([savepath '\data_info'])
     cd([savepath '\data_info'])
     save('info','info');
     save('all_celltypes','all_celltypes');
+    save('mouse','mouse');
     save('imaging_st','imaging_st','-v7.3');
 end
