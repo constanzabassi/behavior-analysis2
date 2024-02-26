929b1e0d2447a6ea2e24f5289956616434a52cb7
constanzabassi <cdb66@pitt.edu>
2024-02-26 10:36:59 -0500

    minor change to ROC

--- ROC/run_ROC.m
+++ ROC/run_ROC.m
@@ -12,7 +12,7 @@ plot_info.colors_celltype = [0.37 0.75 0.49 %light green
                             0 0 0.5]; %dark purple
 
 %everything will be saved inside roc_mdl
-roc_mdl.savestr = 'avg_deconv';
+roc_mdl.savestr = 'avg_dff';
 roc_mdl.event_onset = 141;
 roc_mdl.frames = roc_mdl.event_onset-11:roc_mdl.event_onset-1; %using 10 frames before turn onset (finds avg of these frames)
 roc_mdl.shuff_num = 50;
@@ -26,7 +26,7 @@ roc_mdl.binss = roc_mdl.event_onset-31:roc_mdl.bin_size:roc_mdl.event_onset+30; 
 %% RUN ROC ACROSS DATASETS FINDING MEAN IN BIN roc_mdl.frames
 [roc_mdl] = compute_roc_choice (imaging_st,alignment,roc_mdl,info,plot_info,all_celltypes,1); %last value is whether to shuffle datasets or not
 %% PLOT EXAMPLE CELLS
-ex_mouse = 1;
+ex_mouse = 5;
 alignment.field_to_separate = [1,2]; %1) correct, 2) left turn, 3) condition, 4)is_stim_trial
 alignment.conditions = [3,4]; %of conditions separated list out which ones to plot
 alignment.data_type = 'z_dff';% 'dff', 'z_dff', else it's deconvolved
