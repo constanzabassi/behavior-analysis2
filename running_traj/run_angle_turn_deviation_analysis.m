2df20a0a087cc693d7f422be3d7ab2b542a34ee2
constanzabassi <cdb66@pitt.edu>
2024-03-04 14:12:07 -0500

    update run_heading_trajectory

--- running_traj/run_angle_turn_deviation_analysis.m
+++ running_traj/run_angle_turn_deviation_analysis.m
@@ -1,6 +1,8 @@
 %% analysis of running trajectories/view angle changes based on Green et al 2023
 %get data for each mouse
-m = 1;
+savepath = [info.savepath '\navigation_correction'];
+num_bins = 25;
+for m = 1:length(imaging_st)
 imaging = imaging_st{1,m};
 empty_trials = find(cellfun(@isempty,{imaging.good_trial}));
 good_trials =  setdiff(1:length(imaging),empty_trials); %only trials with all imaging data considered!
@@ -8,20 +10,23 @@ good_trials =  setdiff(1:length(imaging),empty_trials); %only trials with all im
 imaging_array = [imaging(good_trials).movement_in_imaging_time]; %convert to array for easier indexing
 imaging_trial_info = [imaging(good_trials).virmen_trial_info];
 
-%%1) compute mouse's heading deviation (difference between its heading at
-%each timepoint and the mean heading (smoothed- take 25% of shortest
-%trials- compute the median and the circular mean heading at binned
-%distances from the reward)
+%find the log transformed distance from reward
+log_distance = log_transform_reward_distance(imaging_array); %finds eucledian distance
+
+heading_deviation = compute_heading_deviation2(imaging_array, imaging_trial_info, vel_ball{m,1}, log_distance,num_bins); %heading_deviation trials x bins
+
 %also plot heading direction
 median_viewangle = nanmedian([imaging_array.view_angle]); %for some reason it is not zero
 figure(1);clf
-for p = 1:25
-    subplot(5,5,p);
+for p = 1:36
+    subplot(6,6,p);
     p = p;
     hold on;
+    title(num2str(good_trials(p)))
     %plot(rescale(imaging_array(p).x_velocity(imaging_array(p).maze_frames),-1,1));
     plot(vel_ball{m,1}{p,2},'k');
     plot((imaging_array(p).view_angle(imaging_array(p).maze_frames))-median_viewangle,'color',[0.5 0.5 0.5]);
+%     plot(imresize(heading_deviation(p,:),[1,length(imaging_array(p).maze_frames)]),'color',[0.5 0.5 0.5]);
     %plot(rescale(smooth(mean(imaging(good_trials(p)).dff(all_celltypes{1,m}.pv_cells,imaging_array(p).maze_frames)),10,'boxcar'),-1,1),'color',[0.82 0.04 0.04]);
     plot(rescale(smooth(mean(imaging(good_trials(p)).dff(all_celltypes{1,m}.som_cells,imaging_array(p).maze_frames)),3,'boxcar'),-2,2),'color',[0.17 0.35 0.8]);
     
@@ -29,4 +34,15 @@ for p = 1:25
     xlim([1 length(imaging_array(p).maze_frames)])
     ylim([-4 4])
 end
+
+if ~isempty(savepath)
+        mkdir(savepath)
+        cd(savepath)
+        saveas(1,strcat('ex_plots_viewangle_yaw_SOM_dataset',num2str(m),'.svg'));
+        saveas(1,strcat('ex_plots_viewangle_yaw_SOM_dataset',num2str(m),'.png'));
+end
+
+end
+
+
 
