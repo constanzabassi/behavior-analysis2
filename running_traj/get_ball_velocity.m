ac92b31fc2e57de14691af879c48b582663849d6
constanzabassi <cdb66@pitt.edu>
2024-03-04 11:23:52 -0500

    create get_ball_velocity
    doing yaw at first

---
+++ running_traj/get_ball_velocity.m
@@ -0,0 +1,14 @@
+function vel_ball = get_ball_velocity(info,all_frames)
+vel_ball = {};
+for m = 1:length(info.mouse_date)
+    m
+    vel_tr={};
+    load([info.server{1,m} '\Connie\ProcessedData\' info.mouse_date{1,m} '\corrected_velocity.mat']); %pitch(1) roll(2) yaw(3)
+    %conver to randians!
+    converted_yaw = corrected_velocity(3,:)./10.2; %diameter of the ball is about 20.4cm! divide by radius to get radians!
+    for tr = 1:length(all_frames{1,m})
+         vel_tr{tr,1} = corrected_velocity(3,all_frames{1,m}(tr).maze);
+         vel_tr{tr,2} = converted_yaw(1,all_frames{1,m}(tr).maze);
+    end
+    vel_ball{m,1} = vel_tr;
+end
