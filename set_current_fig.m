a527e2752e713ff21a5cbfc2ddbf90108612a972
constanzabassi <cdb66@pitt.edu>
2024-03-06 21:36:08 -0500

    create set_figure

---
+++ set_current_fig.m
@@ -0,0 +1,9 @@
+%% set current figure
+set(gca,'FontName','Arial');
+set(gca,'FontSize',14);
+set(gcf,'Color','w')
+set(gca,'FontName','Arial')
+%set(gca,'Color','k'b)
+set(groot,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'k','k','k'})
+axis square
+movegui(gcf,'center')
