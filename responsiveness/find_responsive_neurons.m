cbaa9e6052320da3c709328a1fef49abb9313daa
constanzabassi <cdb66@pitt.edu>
2024-03-11 10:37:21 -0400

    update to use percentile for significance

--- responsiveness/find_responsive_neurons.m
+++ responsiveness/find_responsive_neurons.m
@@ -1,7 +1,8 @@
-function responsive_neuron = find_responsive_neurons(task_period,current_aligned_dataset,params)
+function [responsive_neuron,responsive_neuron_prctile] = find_responsive_neurons(task_period,current_aligned_dataset,params)
 num_shuff = params.num_shuff;
 p_thr = params.p_thr ;
 responsive_neuron = cell(1,size(task_period,1));
+responsive_neuron_prctile = cell(1,size(task_period,1));
 for neuron = 1:size(current_aligned_dataset,2) %number of neurons
     neuron
     f = squeeze(current_aligned_dataset(:,neuron,:));%squeeze(S_data{a}(i,:,:));
@@ -22,6 +23,10 @@ for neuron = 1:size(current_aligned_dataset,2) %number of neurons
         if pval<p_thr && (nanmean(v(:))-nanmean(v0(:)))>0.01
 %             if pval<p_thr
             responsive_neuron{task_epoch}(end+1) = neuron;
+            
+        end
+        if nanmean(v) > prctile(v0,95)
+            responsive_neuron_prctile{task_epoch}(end+1) = neuron;
         end
     end
 end
--- responsiveness/run_responsive_neurons.m
+++ responsiveness/run_responsive_neurons.m
@@ -15,14 +15,15 @@ alignment.right_padding = [33    33    33    30    23    80];
 
 task_period = define_trial_task_periods(alignment);
 
-%% responsive neurons
+%% responsive neurons- compares activity within and outside task period (same number of frames)
 params.num_shuff = 100;
 params.p_thr = 0.05; 
 for m = 1:length(all_aligned)
     m
     current_aligned_dataset = all_aligned{1,m};
-    responsive_neuron{m} = find_responsive_neurons(task_period,current_aligned_dataset,params);
+    [responsive_neuron{m},responsive_neuron2{m}] = find_responsive_neurons(task_period,current_aligned_dataset,params);
 end
 %% unpack responsive and put into cell type categories
 num_responsive = unpack_responsive(responsive_neuron, all_celltypes); %datasets,task_periods,celltypes
 %% make plot
+boxplot_percent_responsive;
