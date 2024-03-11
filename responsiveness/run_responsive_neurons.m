17cc9c26f177fdadb5106e9e132bbc09090a495d
constanzabassi <cdb66@pitt.edu>
2024-03-11 16:52:48 -0400

    minor changes

--- responsiveness/run_responsive_neurons.m
+++ responsiveness/run_responsive_neurons.m
@@ -2,7 +2,7 @@ alignment.type = 'all'; %'reward','turn','stimulus','ITI'
 alignment.data_type = 'dff'; %'reward','turn','stimulus','ITI'
 
 %% align all datasets inside imaging st
-[aligned_data_all,all_conditions_all,num_trials]= align_multiple_datasets(imaging_st,alignment);
+[aligned_data_all,all_conditions_all,num_trials,alignment]= align_multiple_datasets(imaging_st,alignment);
 
 %% normalize data?
 params.normalization = 'zscore'; %'zscore', 'norm', 'center', 'scale', 'range', or 'medianiqr'
@@ -10,8 +10,6 @@ params.smooth = {}; %{frames,'boxcar' or 'gauss'} or {}
 all_aligned = organize_aligned_datasets(aligned_data_all,params);
 
 %% define stimulus, turn, reward, ITI periods
-alignment.left_padding = [6     1     1    30     4     1]; 
-alignment.right_padding = [33    33    33    30    23    80]; 
 
 task_period = define_trial_task_periods(alignment);
 
@@ -24,6 +22,6 @@ for m = 1:length(all_aligned)
     [responsive_neuron{m},responsive_neuron2{m}] = find_responsive_neurons(task_period,current_aligned_dataset,params);
 end
 %% unpack responsive and put into cell type categories
-num_responsive = unpack_responsive(responsive_neuron, all_celltypes); %datasets,task_periods,celltypes
+num_responsive = unpack_responsive(responsive_neuron2, all_celltypes); %datasets,task_periods,celltypes
 %% make plot
 boxplot_percent_responsive;
