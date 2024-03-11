75473892474fc40a36536c2eff2e02d57a81b020
constanzabassi <cdb66@pitt.edu>
2024-03-11 15:20:19 -0400

    update errorbar plot

--- pop_events/errorbar_events2task.m
+++ pop_events/errorbar_events2task.m
@@ -13,18 +13,18 @@ a = errorbar(1:size(som_data,2), means, std_devs, 'o-', 'LineWidth', 1.5,'Color'
 %% PV
 % Compute means and standard deviations
 pv_data = data.pv;
-means = mean(pv_data);
-std_devs = std(pv_data);
+means2 = mean(pv_data);
+std_devs2 = std(pv_data);
 
 % Plotting
-b= errorbar(1:size(pv_data,2), means, std_devs, 'o-', 'LineWidth', 1.5,'Color',plot_info.colors_celltype(3,:));
+b= errorbar(1:size(pv_data,2), means2, std_devs2, 'o-', 'LineWidth', 1.5,'Color',plot_info.colors_celltype(3,:));
 ylabel('Number of Events');
 xlim([0 5])
 xticks([1:4])
 xticklabels({'Stimulus', 'Turn', 'Reward', 'ITI'});
-legend([a,b],{'SOM','PV'})
 
-set(gca,'fontsize', 14,'box', 'off','FontName','Arial');
+
+
 
 %% COMPUTE STATS
  % Compute means and standard deviations
@@ -36,7 +36,11 @@ set(gca,'fontsize', 14,'box', 'off','FontName','Arial');
     p_values = zeros(1, size(som_data, 2));
     for i = 1:size(som_data, 2)
         [p_values(i), ~] = signrank(som_data(:,i), pv_data(:,i));
+
+        y_val = max([means(i)+std_devs(i); means2(i)+std_devs2(i)]);
+        plot_pval_star(i,y_val+2, p_values(i),[0,0]); %yl(2)+3
     end
+    ylim([-5 round(max(max([means+std_devs; means2+std_devs2])))+5])
 %     %% Mark significant p-values
 %     significant_levels = [0.001, 0.01, 0.05];
 %     for i = 1:length(p_values)
@@ -48,6 +52,7 @@ set(gca,'fontsize', 14,'box', 'off','FontName','Arial');
 %             text(i, max([means_som(i), means_pv(i)]), '*', 'HorizontalAlignment', 'center');
 %         end
 %     end
+legend([a,b],{'SOM','PV'})
 %% Output stats
     stats.p_val_wilcoxon = p_values;
     stats.p_som = p_som;
@@ -58,6 +63,8 @@ set(gca,'fontsize', 14,'box', 'off','FontName','Arial');
     stats.stats_pv = stats_pv;
 
     %%
+
+    set_current_fig; %set with nice font size and square axis
     if ~isempty(savepath)
         mkdir(savepath)
         cd(savepath)
