3e58835e8006feb8c1283c00cb57e01efa97c654
constanzabassi <cdb66@pitt.edu>
2024-03-11 16:52:01 -0400

    update align_multiple to include alignment output

--- align_multiple_datasets.m
+++ align_multiple_datasets.m
@@ -1,10 +1,12 @@
-function [aligned_data_all,all_conditions_all,num_trials]= align_multiple_datasets(imaging_st,alignment)
+function [aligned_data_all,all_conditions_all,num_trials,alignment]= align_multiple_datasets(imaging_st,alignment)
 for m = 1:length(imaging_st)
     m
     imaging = imaging_st{1,m};
     [align_info,alignment_frames,left_padding,right_padding] = find_align_info (imaging,30);
     [aligned_imaging,imaging_array,align_info] = align_behavior_data (imaging,align_info,alignment_frames,left_padding,right_padding,alignment);
     [all_conditions, condition_array_trials] = divide_trials (imaging); %divide trials into all possible conditions
+    alignment.left_padding = left_padding;
+    alignment.right_padding = right_padding;
 
     aligned_data_all{m} = aligned_imaging;
     all_conditions_all{m} = all_conditions;
