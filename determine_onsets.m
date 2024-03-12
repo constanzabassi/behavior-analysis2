7b8b1d2d150629d03d3ada2b995b004e1a6a3c90
constanzabassi <cdb66@pitt.edu>
2024-03-12 16:15:17 -0400

    update determine onsets

--- determine_onsets.m
+++ determine_onsets.m
@@ -2,9 +2,9 @@ function event_onsets = determine_onsets(left_padding,right_padding,event_id)
 temp = [];
 for e = 1:length(event_id)
     if e == 1
-        temp = left_padding(event_id(e))+1;
+        temp = left_padding((e))+1;
     else
-        temp = [temp+left_padding(event_id(e))+1+right_padding(event_id(e-1))];
+        temp = [temp+left_padding((e))+1+right_padding((e-1))];
     end
     event_onsets(e,:) = temp;
 end
