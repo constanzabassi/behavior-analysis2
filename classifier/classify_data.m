0f3cc39c4ff49174b9061ca404bce9a020ee7e3f
constanzabassi <cdb66@pitt.edu>
2024-04-02 11:47:31 -0400

    update mdl params

--- classifier/classify_data.m
+++ classifier/classify_data.m
@@ -6,6 +6,12 @@ alignment.type = 'reward'; %'reward','turn','stimulus','ITI'
 alignment.events = [5]; %1:6 is all
 alignment.single_event = 1; %align to single event == 1 or 1:6 events (concatenated) not 1
 
+%mdl parameter info
+mdl_param.bin = 3; %bin size in terms of frames
+mdl_param.fields_to_balance = [1,2]; %{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
+mdl_param.field_to_predict = 1; %{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
+mdl_param.num_iterations = 25; %number of times to subsample
+mdl_param.data_type = alignment.data_type;
 
 %use alignment info to get this number!
 if alignment.single_event == 1
@@ -13,6 +19,8 @@ if alignment.single_event == 1
 
     mdl_param.event_onset = left_padding(alignment.events)+1;%141; %relative to aligned data this are the events in aligned data:(7,42,77,141,176,201)
     mdl_param.frames_around = -left_padding(alignment.events):right_padding(alignment.events);
+    mdl_param.binns = -left_padding(alignment.events):mdl_param.bin:right_padding(alignment.events); %bins in terms of event onset
+
 else
     [~,~,left_padding,right_padding] = find_align_info (imaging_st{1,1},30);
 
@@ -20,19 +28,11 @@ else
     %updated event onsets! 7 42 77 141 155 180
     frame_length = 150;%for choice; %used to be 281 with update is 260
     mdl_param.frames_around = -mdl_param.event_onset+1:(frame_length)-mdl_param.event_onset+1;%-mdl_param.event_onset+1:mdl_param.event_onset-51 == -140:90; %frames around onset 
+    mdl_param.binns = mdl_param.frames_around(1):mdl_param.bin:mdl_param.frames_around(end); %bins in terms of event onset
 
 end
 
 
-mdl_param.bin = 3; %bin size in terms of frames
-mdl_param.binns = mdl_param.frames_around(1):mdl_param.bin:mdl_param.frames_around(end); %bins in terms of event onset
-mdl_param.fields_to_balance = [1,2]; %{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
-mdl_param.field_to_predict = 1; %{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
-
-
-mdl_param.data_type = alignment.data_type;
-
-mdl_param.num_iterations = 25; %number of times to subsample
 
 plot_info.colors_celltype = [0.37 0.75 0.49 %light green
                             0.17 0.35 0.8  %blue
