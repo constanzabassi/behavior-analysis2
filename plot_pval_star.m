1fba17130207d6960a9cfa8df9db329fae588014
constanzabassi <cdb66@pitt.edu>
2024-05-07 19:28:41 -0400

    update plot_pval_star to input color

--- plot_pval_star.m
+++ plot_pval_star.m
@@ -1,4 +1,4 @@
-function plot_pval_star(x_val, y_val, p, xline_vars,y_val_line_diff)
+function plot_pval_star(x_val, y_val, p, xline_vars,y_val_line_diff,varargin)
 %%% assumes distance between three is x_seq = [-0.2, 0, 0.2];
 
 % Add significance symbols based on p-value
@@ -18,11 +18,13 @@ text_height = yMax;
 
 % Plot significance symbol
 if ~isempty(sig_symbol)
-    text(x_val+mean([xline_vars(1),xline_vars(2)]), text_height, sig_symbol,'HorizontalAlignment', 'center', 'Color', 'k','FontSize',14);
-    
-
-    line([x_val+xline_vars(1), x_val+xline_vars(2)], [y_val-y_val_line_diff, y_val-y_val_line_diff], 'Color', 'k', 'LineWidth', 0.5);
-
+    if nargin > 5 %include this for adding specific color
+        text(x_val+mean([xline_vars(1),xline_vars(2)]), text_height, sig_symbol,'HorizontalAlignment', 'center', 'Color', varargin{1},'FontSize',14);
+        line([x_val+xline_vars(1), x_val+xline_vars(2)], [y_val-y_val_line_diff, y_val-y_val_line_diff], 'Color', varargin{1}, 'LineWidth', 0.5);
+    else
+        text(x_val+mean([xline_vars(1),xline_vars(2)]), text_height, sig_symbol,'HorizontalAlignment', 'center', 'Color', 'k','FontSize',14);
+        line([x_val+xline_vars(1), x_val+xline_vars(2)], [y_val-y_val_line_diff, y_val-y_val_line_diff], 'Color', 'k', 'LineWidth', 0.5);
+    end
 end
 
 % hold off;
