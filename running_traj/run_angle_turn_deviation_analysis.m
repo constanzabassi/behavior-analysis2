a4a550e8f3e304e4c39556732f708f95d8763f4a
constanzabassi <cdb66@pitt.edu>
2024-06-18 16:10:42 -0400

    minor changes to running trajectory

--- running_traj/run_angle_turn_deviation_analysis.m
+++ running_traj/run_angle_turn_deviation_analysis.m
@@ -21,18 +21,26 @@ heading_deviation = compute_heading_deviation2(imaging_array, imaging_trial_info
 %also plot heading direction
 median_viewangle = nanmedian([imaging_array.view_angle]); %for some reason it is not zero
 
+%to get all activity at once
+avg_all = [];for t = good_trials; avg_all = [avg_all,imaging(t).dff(all_celltypes{1,m}.som_cells,:)];end
+headin_all = [];count = 0;for t = 1:length(good_trials); headin_all = [headin_all, imresize(heading_deviation(t,:),[1,length(imaging_array(t).maze_frames)+length(imaging_array(t).reward_frames)+length(imaging_array(t).iti_frames)])]; end
 
 figure(1);clf
 for p = 1:36
-    frames_to_include = [imaging_array(p).maze_frames,imaging_array(p).reward_frames];
+    
     subplot(6,6,p);
-    p = p;
+    p = p+10;
+    frames_to_include = [imaging_array(p).maze_frames,imaging_array(p).reward_frames];
     hold on;
     title(num2str(good_trials(p)))
     plot(rescale(imaging_array(p).x_velocity(frames_to_include),-1,1),'k');
+%     plot(rescale(diff(imaging_array(p).x_velocity(frames_to_include)),-1,1),'k','LineWidth',1);
 %     plot(vel_ball{m,1}{p,2},'k');
+
     plot((imaging_array(p).view_angle(frames_to_include))-median_viewangle,'color',[0.5 0.5 0.5]);
-%     plot(imresize(heading_deviation(p,:),[1,length(imaging_array(p).maze_frames)]),'color',[0.5 0.5 0.5]);
+%     plot(rescale(rad2deg(imaging_array(p).view_angle(frames_to_include))-90,-2,2),'color',[0.5 0.5 0.5],'LineWidth',1);
+
+    plot(imresize(heading_deviation(p,:),[1,length(imaging_array(p).maze_frames)]),'color',[0.7 0.7 0.7]);
     %plot(rescale(smooth(mean(imaging(good_trials(p)).dff(all_celltypes{1,m}.pv_cells,imaging_array(p).maze_frames)),10,'boxcar'),-1,1),'color',[0.82 0.04 0.04]);
     plot(rescale(smooth(mean(imaging(good_trials(p)).dff(all_celltypes{1,m}.som_cells,frames_to_include)),3,'boxcar'),-2,2),'color',[0.17 0.35 0.8]);
     
