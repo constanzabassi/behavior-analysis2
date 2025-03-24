ab59f180c3b90f17f609a009cffc1be3679205d5
constanzabassi <cdb66@pitt.edu>
2025-03-24 14:14:46 -0400

    update run_behavioral_performance

--- behavior/run_performance_analysis.m
+++ behavior/run_performance_analysis.m
@@ -5,10 +5,11 @@ load('V:\Connie\results\behavior_updated\data_info\imaging_st.mat');
 behav_param.fields_to_balance = [3,4];%{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
 alignment.data_type = 'dff';% 'dff', 'z_dff', else it's deconvolved
 alignment.type = 'all'; %'reward','turn','stimulus','ITI'
+chosen_mice = [1:25];
 
 performance = get_opto_performance_simple(imaging_st,behav_param,alignment);
-[behav_param.p_val] = plot_performance(performance(:,chosen_mice),[info.savepath '/performance_analysis']);
-
+[behav_param.p_val] = plot_performance(performance(:,chosen_mice),[params.info.savepath '/performance_analysis']);
+plot_performance_all(performance(1,1:25),[],[params.info.mouseid{:}]);
 %% iterates to balance condition and opto trials
 behav_param.num_iterations = 5;
 behav_param.fields_to_balance = [3,4];%{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
