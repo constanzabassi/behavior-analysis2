f8e26e8cf07f7e1ea4911f13c0c8dbc13fcc1df2
constanzabassi <cdb66@pitt.edu>
2025-03-24 14:20:17 -0400

    update run_performance_analysis

--- behavior/run_performance_analysis.m
+++ behavior/run_performance_analysis.m
@@ -6,27 +6,32 @@ behav_param.fields_to_balance = [3,4];%{'correct'}=1 {'left_turn'}=2 {'condition
 alignment.data_type = 'dff';% 'dff', 'z_dff', else it's deconvolved
 alignment.type = 'all'; %'reward','turn','stimulus','ITI'
 chosen_mice = [1:25];
+save_path = 'V:/Connie/results/opto_sound_2025/';
 
 performance = get_opto_performance_simple(imaging_st,behav_param,alignment);
-[behav_param.p_val] = plot_performance(performance(:,chosen_mice),[params.info.savepath '/performance_analysis']);
-plot_performance_all(performance(1,1:25),[],[params.info.mouseid{:}]);
+plot_performance_all(performance(1,chosen_mice),[save_path '/performance_analysis'],[params.info.mouseid{chosen_mice}]);
+
+%NOW PLOT OPTO VS CONTROL
+chosen_mice = [1:24];
+[behav_param.p_val] = plot_performance(performance(:,chosen_mice),[save_path '/performance_analysis'],[params.info.mouseid{chosen_mice}]);
+
 %% iterates to balance condition and opto trials
-behav_param.num_iterations = 5;
-behav_param.fields_to_balance = [3,4];%{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
-alignment.data_type = 'dff';% 'dff', 'z_dff', else it's deconvolved
-alignment.type = 'all'; %'reward','turn','stimulus','ITI'
-
-performance = get_opto_performance(imaging_st,behav_param,alignment);
-% performance = get_opto_performance_selected_trials(imaging_st,behav_param,alignment,[1:5]); %takes first five after trials are balanced
-
-% make plots
-%take control mouse out
-chosen_mice = setdiff(1:length(imaging_st),find(strcmp(info.mouse_date,'HE1-00\2023-05-30')));
-
-[behav_param.p_val] = plot_performance(performance(:,chosen_mice),[info.savepath '/performance_analysis']);
-save('behav_param','behav_param');
-
-% plot y,x velocity and view angle (abs of x and view angle)
-[behav_param.p_val_alt] = plot_performance_alt(performance(:,chosen_mice),[info.savepath '/performance_analysis']);
-
-plot_performance_all_bar(performance,[info.savepath '/performance_analysis']);
+% behav_param.num_iterations = 5;
+% behav_param.fields_to_balance = [3,4];%{'correct'}=1 {'left_turn'}=2 {'condition'}=3 {'is_stim_trial'}=4
+% alignment.data_type = 'dff';% 'dff', 'z_dff', else it's deconvolved
+% alignment.type = 'all'; %'reward','turn','stimulus','ITI'
+% 
+% performance = get_opto_performance(imaging_st,behav_param,alignment);
+% % performance = get_opto_performance_selected_trials(imaging_st,behav_param,alignment,[1:5]); %takes first five after trials are balanced
+% 
+% % make plots
+% %take control mouse out
+% chosen_mice = setdiff(1:length(imaging_st),find(strcmp(info.mouse_date,'HE1-00\2023-05-30')));
+% 
+% [behav_param.p_val] = plot_performance(performance(:,chosen_mice),[info.savepath '/performance_analysis']);
+% save('behav_param','behav_param');
+% 
+% % plot y,x velocity and view angle (abs of x and view angle)
+% [behav_param.p_val_alt] = plot_performance_alt(performance(:,chosen_mice),[info.savepath '/performance_analysis']);
+% 
+% plot_performance_all_bar(performance,[info.savepath '/performance_analysis']);
