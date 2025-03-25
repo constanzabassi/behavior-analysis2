984b4966e087c9596e8097f46a8beba4d4c130bc
constanzabassi <cdb66@pitt.edu>
2025-03-24 22:10:09 -0400

    update get_silhouettes with dataset plots

--- red_cells/get_red_silhouettes.m
+++ red_cells/get_red_silhouettes.m
@@ -1,14 +1,15 @@
 %% get silhouette scores across all datasets!
 function [all_sil,missing_data] = get_red_silhouettes(info,plot_info,save_data_directory)
 all_sil = []; missing_data =[];
-for m = 1:length(info.mouse_date)
-    m
+all_silhouettes = {}; total_pyr = [];
+for dataset_index = 1:length(info.mouse_date)
+    dataset_index
     temp = [];
-    mm = info.mouse_date(m)
-    mm = mm{1,1};
-    ss = info.server(m);
+    datasets_id = info.mouse_date(dataset_index)
+    datasets_id = datasets_id{1,1};
+    ss = info.server(dataset_index);
     ss = ss {1,1};
-    load(strcat(num2str(ss),'/Connie/ProcessedData/',num2str(mm),'/dual_red/clustering_info.mat')); %load clustering info!
+    load(strcat(num2str(ss),'/Connie/ProcessedData/',num2str(datasets_id),'/dual_red/clustering_info.mat')); %load clustering info!
 
     red_vects = find(clustering_info.redvect); 
     celltype_ids = clustering_info.cellids(red_vects);
@@ -29,9 +30,9 @@ for m = 1:length(info.mouse_date)
         
         % Uncertain values to locate in the original sequence
         uncertain_positions = clustering_info.uncertain; % Example: [218, 251, 261]
-        if contains(mm,'2023-07-07')
+        if contains(datasets_id,'2023-07-07')
             uncertain_positions = uncertain_positions(2:3); %union(clustering_info.uncertain,clustering_info.excluded);
-        elseif contains(mm,'2023-03-31')
+        elseif contains(datasets_id,'2023-03-31')
             uncertain_positions = uncertain_positions(2);
         end
         % Find where these uncertain values **would have been** in `redvect`
@@ -57,9 +58,15 @@ for m = 1:length(info.mouse_date)
         temp(1,:)= clustering_info.used_silhouettes;
         temp(2,:) = celltype_ids;
         all_sil = [all_sil, temp];
-
-        missing_data = [missing_data,info.mouse_date(m)];
+        
+        missing_data = [missing_data,info.mouse_date(dataset_index)];
     end
+    som_indices = find(temp(2,:) == 1);
+    pv_indices = find(temp(2,:) == 2);
+    all_silhouettes{dataset_index,1} = temp(1,som_indices); %SOM
+    all_silhouettes{dataset_index,2} = temp(1,pv_indices); %PV
+    total_pyr =[ total_pyr ,length(find(clustering_info.cellids == 0))];
+
 end
 
 %% make histogram of silhoutte scores across celltypes!
@@ -118,6 +125,62 @@ xticklabels({'SOM','PV'})
 xlim([0 3])
 set_current_fig;
 
+%% make plots divided by datasets
+n_mice = length(all_silhouettes);
+figure(91);clf;
+for celltype= 1:2
+    hold on
+    %SOM
+    h = boxplot(cellfun(@nanmean ,{all_silhouettes{:,celltype}}), 'position', celltype, 'width', .7, 'colors',  plot_info.colors_celltype(celltype+1,:),'symbol', 'o');
+    %set line width
+    out_line = findobj(h, 'Tag', 'Outliers');
+    set(out_line, 'Visible', 'off');
+    hh = findobj('LineStyle','--','LineWidth',1); 
+    set(h(1:6), 'LineStyle','-','LineWidth',1.5);
+    for m = 1:n_mice
+        jitter = (rand-.5) *.5;
+    %     plot(1+jitter, mean(all_silhouettes{m,1})  ,'o','MarkerFaceColor', plot_info.colors_celltype(2,:),0.2);
+        scatter(celltype+jitter, mean(all_silhouettes{m,1}), 30, ...
+                            plot_info.colors_celltype(celltype+1,:), 'o', 'filled', ...
+                            'MarkerFaceAlpha', 0.4)
+    end
+    ylim([.8 1])
+    
+    hold off
+end
+xlim([0,3])
+xticks([1,2])
+xticklabels({'SOM', 'PV'});
+ylabel('Silhouette Score')
+box off
+set(gcf,'units','points','position',[100,100,150,150])
+set_current_fig;
+
+p_val = signrank(cellfun(@nanmean ,{all_silhouettes{:,1}}),cellfun(@nanmean ,{all_silhouettes{:,2}}));
+red_stats.p_val_sign_datasets = p_val;
+
+figure(92);clf;
+mouse_means = [cellfun(@length ,{all_silhouettes{:,1}});cellfun(@length ,{all_silhouettes{:,2}})];
+for celltype= 1:2
+    mean_cel = mean(cellfun(@length ,{all_silhouettes{:,celltype}}));
+    err = std(cellfun(@length ,{all_silhouettes{:,celltype}})) / sqrt(length(all_silhouettes));
+    hold on
+    errorbar(celltype, mean_cel, err, 'o', ...
+        'Color', plot_info.colors_celltype(celltype+1,:), ...
+        'LineWidth', 1, 'MarkerSize', 2,'MarkerFaceColor', plot_info.colors_celltype(celltype+1,:));
+    % Plot connected line for this mouse
+    plot([1+.2,2-.2], mouse_means, '-', 'Color', ...
+        [0.5,0.5,0.5, 0.3], 'LineWidth', 1)
+
+end
+xlim([0,3])
+xticks([1,2])
+xticklabels({'SOM', 'PV'});
+box off
+set(gcf,'units','points','position',[100,100,150,150])
+ylabel('Cell Counts')
+set_current_fig;
+
 if ~isempty(save_data_directory)
     mkdir(save_data_directory)
     cd(save_data_directory)
@@ -131,7 +194,12 @@ if ~isempty(save_data_directory)
     saveas(90,[image_string '_datasets_scattersem.fig']);
     saveas(90,[image_string '_datasets_scattersem.pdf']);
 
+    exportgraphics(figure(92),strcat('cell_counts_connected_lines_n',num2str(n_mice),'_datasets.pdf'), 'ContentType', 'vector');
+    exportgraphics(figure(91),strcat('Silouette_scores_n',num2str(n_mice),'_datasets.pdf'), 'ContentType', 'vector');
+
+
     save('all_sil','all_sil');
+    save('all_silhouettes','all_silhouettes');
     save('missing_data','missing_data')
     save('red_stats','red_stats');
 end
