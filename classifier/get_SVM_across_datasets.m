1d539fffc75be0d9ed92127414a37a33e2df8aee
constanzabassi <cdb66@pitt.edu>
2025-04-02 14:02:08 -0400

    update get svm to find mean across folds

--- classifier/get_SVM_across_datasets.m
+++ classifier/get_SVM_across_datasets.m
@@ -4,32 +4,50 @@ output_mat2 = [];
 for m = 1:length(info.chosen_mice)
     
 figure(m);clf;
-
-for ce = 4
+if length(plot_info.colors)>2
+    total_celltypes = 4;
+end
+for ce = 1:total_celltypes
      hold on;
     %title(info.mouse_date{1,m})
-    
+
     temp1 = vertcat(acc{1,m}{1:10,1:50,ce});
     temp2 = vertcat(shuff_acc{1,m}{1:10,1:50,ce});
-    output_mat{m,ce}.accuracy = temp1; %gives num subsample iterations x timepoints
-    output_mat{m,ce}.shuff_accuracy = temp2;
+    
+    folds_mean = []; %cellfun(@mean, acc{1,m}(1:10,:,ce));
+    folds_mean_shuff = []; %cellfun(@mean, shuff_acc{1,m}(1:10,:,ce));
+    for i=0:9
+        folds_mean =[folds_mean; mean(temp1(i*10+1:i*10+10,:))];
+        folds_mean_shuff =[folds_mean_shuff; mean(temp2(i*10+1:i*10+10,:))];
+    end
+    
+    output_mat{m,ce}.accuracy = folds_mean; %gives mean across folds x timepoints
+    output_mat{m,ce}.shuff_accuracy = folds_mean_shuff;
     
     % find squared error from the mean
     SEM= std(output_mat{m,ce}.accuracy)/sqrt(size(output_mat{m,ce}.accuracy,1));
-    h1(1) = shadedErrorBar(1:size(output_mat{m,ce}.accuracy,2),mean(output_mat{m,ce}.accuracy,1), SEM, 'lineProps',{'color', plot_info.colors(1,:)});
+    h1(1) = shadedErrorBar(1:size(output_mat{m,ce}.accuracy,2),mean(output_mat{m,ce}.accuracy,1), SEM, 'lineProps',{'color', plot_info.colors(ce,:)});
 
     SEM2= std(output_mat{m,ce}.shuff_accuracy)/sqrt(size(output_mat{m,ce}.shuff_accuracy,1));
     shadedErrorBar(1:size(output_mat{m,ce}.shuff_accuracy,2),mean(output_mat{m,ce}.shuff_accuracy,1), SEM2, 'lineProps',{'color', [0.2 0.2 0.2]*ce});
 
     if nargin > 5
+
         temp3 = vertcat(varargin{1,1}{1,1}{1,m}{1:10,1:50,ce});
         temp4 = vertcat(varargin{1,1}{1,2}{1,m}{1:10,1:50,ce});
-        output_mat2{m,ce}.accuracy = temp3; %gives num subsample iterations x timepoints
-        output_mat2{m,ce}.shuff_accuracy = temp4;
+        
+        folds_mean_passive = []; %cellfun(@mean, acc{1,m}(1:10,:,ce));
+        folds_mean_shuff_passive = []; %cellfun(@mean, shuff_acc{1,m}(1:10,:,ce));
+        for i=0:9
+            folds_mean_passive =[folds_mean_passive; mean(temp3(i*10+1:i*10+10,:))];
+            folds_mean_shuff_passive =[folds_mean_shuff_passive; mean(temp4(i*10+1:i*10+10,:))];
+        end
+        output_mat2{m,ce}.accuracy = folds_mean_passive; %gives fold means x timepoints
+        output_mat2{m,ce}.shuff_accuracy = folds_mean_shuff_passive;
 
         % find squared error from the mean
         SEM= std(output_mat2{m,ce}.accuracy)/sqrt(size(output_mat2{m,ce}.accuracy,1));
-        h1(2) = shadedErrorBar(1:size(output_mat2{m,ce}.accuracy,2),mean(output_mat2{m,ce}.accuracy,1), SEM, 'lineProps',{'color', plot_info.colors(2,:)});
+        h1(2) = shadedErrorBar(1:size(output_mat2{m,ce}.accuracy,2),mean(output_mat2{m,ce}.accuracy,1), SEM, 'lineProps',{'color', plot_info.colors(ce+total_celltypes,:)});
 
         SEM2= std(output_mat2{m,ce}.shuff_accuracy)/sqrt(size(output_mat2{m,ce}.shuff_accuracy,1));
         shadedErrorBar(1:size(output_mat2{m,ce}.shuff_accuracy,2),mean(output_mat2{m,ce}.shuff_accuracy,1), SEM2, 'lineProps',{'color', [0.2 0.2 0.2]*ce});
